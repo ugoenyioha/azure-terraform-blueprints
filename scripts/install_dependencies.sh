20676b0f6d8b3a7ad47d3237525e8d9d77df4bfa
Ugo Enyioha <ugo.enyioha@gmail.com>
2017-11-11 08:41:49 -0800

    no message

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,4 +11,4 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy ma
      sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+sudo apt-get update && sudo apt-get install azure-cli=2.0.8-1
