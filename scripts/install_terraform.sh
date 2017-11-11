eb3efe1e5060db20891b2001b800c236054cf43f
Ugo Enyioha <ugo.enyioha@gmail.com>
2017-11-10 22:25:01 -0800

    sudo required

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -1,4 +1,4 @@
 curl -fSL "https://releases.hashicorp.com/terraform/0.10.8/terraform_0.10.8_linux_amd64.zip" -o terraform.zip
 unzip terraform.zip -d /opt/terraform
-ln -s /opt/terraform/terraform /usr/bin/terraform
+sudo ln -s /opt/terraform/terraform /usr/bin/terraform
 rm -f terraform.zip
