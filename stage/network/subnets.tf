d88265b4e723f71b8dedb38bb92faf59fc4460c6
Ugo Enyioha <ugo.enyioha@gmail.com>
2017-11-12 12:47:00 -0800

    no message

--- stage/network/subnets.tf
+++ stage/network/subnets.tf
@@ -1,26 +1,26 @@
-# resource "azurerm_virtual_network" "petshop-network" {
-#   name = "petshop-network"
-#  address_space = ["10.0.0.0/16"]
-#  location = "West US 2"
-#  resource_group_name = "${var.azure_resource_group_name}"
+resource "azurerm_virtual_network" "petshop-network" {
+  name = "petshop-network"
+  address_space = ["10.0.0.0/16"]
+  location = "West US 2"
+  resource_group_name = "${var.azure_resource_group_name}"
 
 #  subnet {
 #    name = "loadbalancers"
 #    address_prefix= "10.0.1.0/24"
 #  }
 
-#  subnet {
-#    name = "backend-servers"
-#    address_prefix= "10.0.3.0/24"
-#  }
+  subnet {
+    name = "backend-servers"
+    address_prefix= "10.0.3.0/24"
+  }
 
 #  subnet {
 #    name = "databases"
 #    address_prefix = "10.0.4.0/24"
 #  }
 
-#  tags {
-#    environment = "staging"
-#  }
-# }
+  tags {
+    environment = "staging"
+  }
+}
 
