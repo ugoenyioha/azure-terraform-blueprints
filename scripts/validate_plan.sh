cdecfc9441f5015c72e4914ee8b8a6c62a9c6c24
Ugo Enyioha <ugo.enyioha@gmail.com>
2017-11-11 20:07:34 -0800

    no message

---
+++ scripts/validate_plan.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pushd stage/global/
+terraform init -backend-config="storage_account_name=${STORAGE_ACCOUNT_NAME}" \
+             -backend-config="container_name=${CONTAINER_NAME}"  \
+             -backend-config="key=${STATE_KEY}" \
+             -backend-config="resource_group_name=${RESOURCE_GROUP_NAME}" \
+             -backend-config="access_key=${STORAGE_ACCESS_KEY}"
+terraform plan
