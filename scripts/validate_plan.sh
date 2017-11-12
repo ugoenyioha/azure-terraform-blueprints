2e2b984caaa14cb5b3975d6ba94b1819e08f7581
Ugo Enyioha <ugo.enyioha@gmail.com>
2017-11-12 04:29:59 -0800

    no message

--- scripts/validate_plan.sh
+++ scripts/validate_plan.sh
@@ -2,20 +2,20 @@
 
 echo "Validating Base Environment..."
 pushd stage/global/
-terraform init -backend-config="storage_account_name=${STORAGE_ACCOUNT_NAME}" \
-             -backend-config="container_name=${CONTAINER_NAME}"  \
-             -backend-config="key=${STATE_KEY}" \
-             -backend-config="resource_group_name=${RESOURCE_GROUP_NAME}" \
-             -backend-config="access_key=${STORAGE_ACCESS_KEY}"
+terraform init -backend-config="storage_account_name=${AZURE_STORAGE_ACCOUNT_NAME}" \
+             -backend-config="container_name=${AZURE_CONTAINER_NAME}"  \
+             -backend-config="key=${AZURE_STATE_KEY}" \
+             -backend-config="resource_group_name=${AZURE_RESOURCE_GROUP_NAME}" \
+             -backend-config="access_key=${AZURE_STORAGE_ACCESS_KEY}"
 terraform plan
 popd
 
 echo "Validating Network..."
 pushd stage/network/
-terraform init -backend-config="storage_account_name=${STORAGE_ACCOUNT_NAME}" \
-             -backend-config="container_name=${CONTAINER_NAME}"  \
-             -backend-config="key=${STATE_KEY}" \
-             -backend-config="resource_group_name=${RESOURCE_GROUP_NAME}" \
-             -backend-config="access_key=${STORAGE_ACCESS_KEY}"
+terraform init -backend-config="storage_account_name=${AZURE_STORAGE_ACCOUNT_NAME}" \
+             -backend-config="container_name=${AZURE_CONTAINER_NAME}"  \
+             -backend-config="key=${AZURE_STATE_KEY}" \
+             -backend-config="resource_group_name=${AZURE_RESOURCE_GROUP_NAME}" \
+             -backend-config="access_key=${AZURE_STORAGE_ACCESS_KEY}"
 terraform plan
 popd
