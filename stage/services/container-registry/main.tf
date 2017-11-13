0cae07daff4cdd3727900eb0f442f45011467eb3
Ugo Enyioha <ugo.enyioha@gmail.com>
2017-11-12 23:31:00 -0800

    rename container registry

--- stage/services/container-registry/main.tf
+++ stage/services/container-registry/main.tf
@@ -32,7 +32,7 @@ data "terraform_remote_state" "staging_network" {
 }
 
 resource "azurerm_container_registry" "PetclinicContainerRegistry" {
-  name                = "PetclinicContainerRegistry"
+  name                = "petclinic"
   resource_group_name = "${var.azure_resource_group_name}"
   location            = "westus2"
   admin_enabled       = true
