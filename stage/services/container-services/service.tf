07507b855a55039df6c8392b3434d2147cfa1c73
Ugo Enyioha <ugo.enyioha@gmail.com>
2017-11-13 20:03:52 -0800

    deploy kubernetes cluster

--- stage/services/container-services/service.tf
+++ stage/services/container-services/service.tf
@@ -1,12 +1,16 @@
 resource "azurerm_resource_group" "petcliniccluster" {
   location = "westus2"
   name = "petcliniccluster"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_container_service" "petcliniccontainerservice" {
   name                   = "petcliniccontainerservice"
   location               = "westus2"
-  resource_group_name = "petcliniccluster"
+  resource_group_name = "${azurerm_resource_group.petcliniccluster.name}"
   orchestration_platform = "Kubernetes"
 
   master_profile {
@@ -21,12 +25,13 @@ resource "azurerm_container_service" "petcliniccontainerservice" {
       key_data = "${var.azure_kube_ssh_pubkey}"
     }
   }
+  g
 
   agent_pool_profile {
     name       = "default"
-    count      = 1
-    dns_prefix = "petclinicagent"
-    vm_size    = "Standard DS2 v2"
+    count      = 3
+    dns_prefix = "acctestagent1"
+    vm_size    = "Standard_A0"
   }
 
   service_principal {
